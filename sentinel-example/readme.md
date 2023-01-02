794e0849e8b654d3a9ee565dece65b0951be3651
zl <zhaolin9810@gmail.com>
2023-01-02 23:40:49 +0800

    初始化sential-example

---
+++ sentinel-example/readme.md
@@ -0,0 +1,8 @@
+```shell script
+ java -Dserver.port=8080 -Dcsp.sentinel.dashboard.server=localhost:8080 -Dproject.name=sentinel-dashboard -jar sentinel-dashboard-1.8.6.jar
+```
+
+client增加参数
+```shell script
+-Dcsp.sentinel.dashboard.server=127.0.0.1:8080
+```
---
+++ sentinel-example/src/main/java/io/zl/ResourceMain.java
@@ -0,0 +1,47 @@
+package io.zl;
+
+import com.alibaba.csp.sentinel.Entry;
+import com.alibaba.csp.sentinel.SphU;
+import com.alibaba.csp.sentinel.annotation.SentinelResource;
+import com.alibaba.csp.sentinel.slots.block.BlockException;
+import com.alibaba.csp.sentinel.slots.block.RuleConstant;
+import com.alibaba.csp.sentinel.slots.block.flow.FlowRule;
+import com.alibaba.csp.sentinel.slots.block.flow.FlowRuleManager;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ResourceMain {
+    public static void main(String[] args) {
+        // 配置规则.
+        initFlowRules();
+
+        while (true) {
+            // 1.5.0 版本开始可以直接利用 try-with-resources 特性
+            try (Entry entry = SphU.entry("HelloWorld")) {
+                // 被保护的逻辑
+//                System.out.println("hello world");
+                helloWorld();
+            } catch (BlockException ex) {
+                // 处理被流控的逻辑
+                System.out.println("blocked!");
+            }
+        }
+    }
+
+    @SentinelResource(value = "HelloWorld")
+    static public void helloWorld(){
+        System.out.println("hello world");
+    }
+
+    private static void initFlowRules(){
+        List<FlowRule> rules = new ArrayList<>();
+        FlowRule rule = new FlowRule();
+        rule.setResource("HelloWorld");
+        rule.setGrade(RuleConstant.FLOW_GRADE_QPS);
+        // Set limit QPS to 20.
+        rule.setCount(20);
+        rules.add(rule);
+        FlowRuleManager.loadRules(rules);
+    }
+}

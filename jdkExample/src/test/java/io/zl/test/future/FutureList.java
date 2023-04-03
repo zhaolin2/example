c40a6e51603ecdb3b7c9ac3762b1b48624dce7a7
zl <zhaolin9810@gmail.com>
2023-04-04 07:54:13 +0800

    增加future对应聚合器

---
+++ jdkExample/src/test/java/io/zl/test/future/FutureList.java
@@ -0,0 +1,28 @@
+package io.zl.test.future;
+
+
+import java.util.ArrayList;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.Future;
+import java.util.stream.Collectors;
+
+public class FutureList {
+    public static void main(String[] args) {
+
+        ArrayList<String> strings = new ArrayList<>();
+        strings.add("123");
+        strings.add("345");
+
+
+        CompletableFuture<Void>[] futures = strings.stream().map(FutureList::getAsync).toArray(CompletableFuture[]::new);
+        CompletableFuture.allOf(futures);
+
+        System.out.println("future执行完毕");
+    }
+
+    static public CompletableFuture<Void> getAsync(String s){
+        return CompletableFuture.runAsync(()->{
+            System.out.println("s:"+s);
+        });
+    }
+}
